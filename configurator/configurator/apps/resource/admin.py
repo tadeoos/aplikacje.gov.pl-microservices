cae7e568e92b7dd6e0fc6147ea9db4826fae01e4
Tadek Teleżyński <tadekte@gmail.com>
2017-04-13 11:30:07 +0200

    make migrations

--- configurator/configurator/apps/resource/admin.py
+++ configurator/configurator/apps/resource/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(StringResource)
+admin.site.register(IntResource)
+admin.site.register(ListResource)
+admin.site.register(DictResource)
+admin.site.register(DictResourceEntry)
