53e0cc4cd45470cc3fd1ecfe7f32254fbe1b1dec
Tadek Teleżyński <tadekte@gmail.com>
2017-04-19 14:16:09 +0200

    temp

--- configurator/configurator/apps/resource/models.py
+++ configurator/configurator/apps/resource/models.py
@@ -84,3 +84,6 @@ class DictResourceEntry(models.Model):
     dictionary = models.ForeignKey(DictResource, related_name='entries')
     key = models.TextField()
     value = models.ForeignKey(Resource)
+
+    def __str__(self):
+        return '{} : {}'.format(self.key, self.value)
